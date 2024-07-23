31b545f59978fa83a5b4a16260fae0e00621c7ae
Jackson Dugan <dugan102@d.umn.edu>
2024-07-23 09:30:44 -0500

    Completed the practice view, it now has a nice controller.

--- CoreBluetoothPlayground/PracticeView.swift
+++ CoreBluetoothPlayground/PracticeView.swift
@@ -7,25 +7,65 @@
 
 import SwiftUI
 
+struct CustomCircleButton: View {
+    let action: () -> Void
+    private let circleSize: CGFloat = 75
+
+    var body: some View {
+        Button(action: action) {
+            Image(systemName: "circle")
+                .font(.system(size: circleSize))
+        }
+    }
+}
+
 struct PracticeView: View {
     @ObservedObject var bluetoothViewModel: BluetoothViewModel
     var body: some View {
-        Text("Hi?")
+        let circleSize: CGFloat = 75
         
-        Button(action: {
-                        if let peripheral = bluetoothViewModel.connectedPeripherals.first {
-                            let data = Data([0x01]) // Sending a single byte with value 1 Data([0x01])
-                            bluetoothViewModel.sendData(data, to: peripheral)
-                        }
-                    }) {
-                        Text("Send 1 to Peripheral")
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-        
+        VStack (spacing: 8) {
+            
+            Spacer()
+            
+            HStack {
+                Spacer()
+                
+                CustomCircleButton(action: {sendToHM10(data: Data([0x01]))})
+                
+                Spacer()
+            }
+            
+            HStack {
+                
+                CustomCircleButton(action: {sendToHM10(data: Data([0x02]))})
+                
+                Spacer().frame(width: 91)
+                
+                CustomCircleButton(action: {sendToHM10(data: Data([0x03]))})
+                
+            }
+            
+            HStack {
+                Spacer()
+                
+                CustomCircleButton(action: {sendToHM10(data: Data([0x04]))})
+                
+                Spacer()
+            }
+            
+            Spacer()
+            
+        }
     }
+    
+    func sendToHM10 (data: Data) {
+        if let peripheral = bluetoothViewModel.connectedPeripherals.first {
+            
+            bluetoothViewModel.sendData(data, to: peripheral)
+        }
+    }
+    
 }
 
 #Preview {
