0c91fe704f4ddcf590bac5d2fce1add4b9b2aec4
Jackson Dugan <dugan102@d.umn.edu>
2024-07-24 08:47:56 -0500

    added an extra functionality to practice view button that notifies when button is let go of or pressed up.

--- CoreBluetoothPlayground/PracticeView.swift
+++ CoreBluetoothPlayground/PracticeView.swift
@@ -8,49 +8,71 @@
 import SwiftUI
 
 struct CustomCircleButton: View {
-    let action: () -> Void
+    let onTouchDown: () -> Void
+    let onTouchUp: () -> Void
     private let circleSize: CGFloat = 75
 
+    @State private var isPressed = false
+
     var body: some View {
-        Button(action: action) {
+        Button(action: {}) {
             Image(systemName: "circle")
                 .font(.system(size: circleSize))
+                .scaleEffect(isPressed ? 0.95 : 1.0) // Slight scale effect to indicate pressing
         }
+        .simultaneousGesture(
+            DragGesture(minimumDistance: 0)
+                .onChanged { _ in
+                    if !isPressed {
+                        isPressed = true
+                        onTouchDown()
+                        print("down")
+                    }
+                }
+                .onEnded { _ in
+                    isPressed = false
+                    onTouchUp()
+                    print("up")
+                }
+        )
     }
 }
 
 struct PracticeView: View {
     @ObservedObject var bluetoothViewModel: BluetoothViewModel
     @State var Speed: Double = 0
+
     var body: some View {
-        
-        VStack (spacing: 8) {
-            
+        VStack(spacing: 8) {
             Spacer()
             
             HStack {
                 Spacer()
-                
-                CustomCircleButton(action: { sendToHM10(buttonNumber: 1, speed: UInt8(Speed)) })
-                
+                CustomCircleButton(
+                    onTouchDown: { sendToHM10(buttonNumber: 1, speed: UInt8(Speed)) },
+                    onTouchUp: { sendToHM10(buttonNumber: 0, speed: UInt8(Speed)) }
+                )
                 Spacer()
             }
             
             HStack {
-                
-                CustomCircleButton(action: { sendToHM10(buttonNumber: 2, speed: UInt8(Speed)) })
-                
+                CustomCircleButton(
+                    onTouchDown: { sendToHM10(buttonNumber: 2, speed: UInt8(Speed)) },
+                    onTouchUp: { sendToHM10(buttonNumber: 0, speed: UInt8(Speed)) }
+                )
                 Spacer().frame(width: 91)
-                
-                CustomCircleButton(action: { sendToHM10(buttonNumber: 3, speed: UInt8(Speed)) })
-                
+                CustomCircleButton(
+                    onTouchDown: { sendToHM10(buttonNumber: 3, speed: UInt8(Speed)) },
+                    onTouchUp: { sendToHM10(buttonNumber: 0, speed: UInt8(Speed)) }
+                )
             }
             
             HStack {
                 Spacer()
-                
-                CustomCircleButton(action: { sendToHM10(buttonNumber: 4, speed: UInt8(Speed)) })
-                
+                CustomCircleButton(
+                    onTouchDown: { sendToHM10(buttonNumber: 4, speed: UInt8(Speed)) },
+                    onTouchUp: { sendToHM10(buttonNumber: 0, speed: UInt8(Speed)) }
+                )
                 Spacer()
             }
             
@@ -58,25 +80,19 @@ struct PracticeView: View {
             
             HStack {
                 Spacer()
-                
                 Slider(value: $Speed, in: 0...255)
-                
                 Spacer()
             }
             
-            
-            
             Spacer()
-            
         }
     }
     
     func sendToHM10(buttonNumber: UInt8, speed: UInt8) {
-            if let peripheral = bluetoothViewModel.connectedPeripherals.first {
-                bluetoothViewModel.sendData(buttonNumber: buttonNumber, speed: speed, to: peripheral)
-            }
+        if let peripheral = bluetoothViewModel.connectedPeripherals.first {
+            bluetoothViewModel.sendData(buttonNumber: buttonNumber, speed: speed, to: peripheral)
         }
-    
+    }
 }
 
 #Preview {
